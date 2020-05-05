8bbfb6328f11ab8d5915c76fb2c5e852f3f6d4c5
agreysrivastava <35161379+agreysrivastava@users.noreply.github.com>
2020-05-05 16:08:32 +0530

    Update Comparator Interface Usage.java

--- projlambda/Comparator Interface Usage.java
+++ projlambda/Comparator Interface Usage.java
@@ -10,14 +10,14 @@ public <Player> int compare(Player p1,Player p2){
         else
             return p1.name.compareTo(p2.name);
     }*/
-class Checker implements Comparator<Player>{
+class Checker implements Comparator<Player>{//For descending order
 
     @Override
     public int compare(Player p1, Player p2) {
         if(p1.score>p2.score)
-            return 1;
-        else if(p2.score>p1.score)
             return -1;
+        else if(p2.score>p1.score)
+            return 1;
         else
             return p1.name.compareTo(p2.name);
     }
